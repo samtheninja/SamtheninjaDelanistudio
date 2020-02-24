8abd0d006d820c22393f9aedfb7165f20eab0f01
Samuel-Mwaura <mr.s.n.mwaura@gmail.com>
2020-02-24 14:51:09 +0300

    adde toggle functionality for what we do

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-$(document).ready(function(){
+$().ready(function(){
+  $(".clickme1").click(function(){
+    $(".doa").toggle();
+    $(".do1").toggle();
+  });
+  $(".clickme2").click(function(){
+    $(".dob").toggle();
+    $(".do2").toggle();
+  });
+  $(".clickme3").click(function(){
+    $(".doc").toggle();
+    $(".do3").toggle();
+  });
+});
+
+$().ready(function()  {
+  $("#m").mouseenter(function(){
+   $(".overlay").show();
+   }).mouseleave(function(){
+     $(".overlay").hide();
+   });
+});
